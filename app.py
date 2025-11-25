b42ba84e5a6c760b84e8b6f996d92e63d1d419c2
mngoh <mngoh8@gmail.com>
2025-11-25 15:59:03 -0500

    updates

--- app.py
+++ app.py
@@ -12,29 +12,55 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
+
+### SECTION 1: ###
 # Load Data
-st.write("### To Get Started Upload Your Data Below")         
+st.write("# To Get Started Upload Your Data Below")         
 
 uploaded_file = st.file_uploader("Upload your CSV file", type=["csv"])
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
-    # Create 2 Cols 
-    col1, col2 = st.columns([1, 1])   
+      
     
-    ### Column 1
-    with col1:
-        st.write("### Preview of Data")         
-        st.dataframe(df.head(10))
+    # Title 
+    st.write('## 1. Your Data at a Glace')
+    
+    # Create a single row with 5 evenly sized columns
+    cols = st.columns(3)
 
+    # Define the content for each box
+    box_contents = [
+        ("Total rows", f"{len(df)}"),
+        ("NAs", f"{df.isna().sum().count()}"),
+        ("Count of Columns", len(df.columns)),
+    ]
+
+    # Loop over columns and content to keep uniformity
+    for col, (title, value) in zip(cols, box_contents):
+        with col:
+            box = st.container(border=True, horizontal=True, horizontal_alignment='center')
+            box.write(f"### {title}")
+            if value:
+                box.write(f"### {value}")
+
+    ### SECTION XX: ###
+    # Create 2 Cols 
+    st.write("## 2. Preview of Your Data")   
+    selector, selector2 = st.columns([1,1])
+    with selector:    
+        selected_col = st.selectbox("Select a column to plot", df.columns)
+    with selector2:    
+        count_n = st.select_slider("Count of Categories to Display", options=[x for x in range(0,50,5)])
+      
+
+    col1, col2 = st.columns([1, 1]) 
+    # Col 1
+    with col1:
+        st.dataframe(df.head(10))
 
     ### Column 2    
     with col2:
-        selector, selector2 = st.columns([1,1])
-        with selector:    
-            selected_col = st.selectbox("Select a column to plot", df.columns)
-        with selector2:    
-            count_n = st.select_slider("Count of Categories to Display", options=[x for x in range(0,50,5)])
         if selected_col:
             if count_n == 0: 
                 count_n = 1
@@ -59,16 +85,14 @@ if uploaded_file is not None:
                 ax.set_title(f"Frequency of {selected_col} Feature")
                 st.pyplot(fig, width='content')
 
+    ### Next Section    
+    st.write("## 3. Interactions Within Data")   
+    cola, colb = st.columns([1,1])
+    with cola:    
+        cola = st.selectbox("Column A", df.columns)
+    with colb:    
+        colb = st.selectbox("Column B", df.columns)    
+
+
 else:
     st.info("Please upload a CSV file to begin.")
-
-
-
-### Next Section 
-    
-st.write("### Interaction Between Columns")   
-cola, colb = st.columns([1,1])
-with cola:    
-    cola = st.selectbox("Column A", df.columns)
-with colb:    
-    colb = st.selectbox("Column B", df.columns)    
