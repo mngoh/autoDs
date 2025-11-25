f03721e14a61036ca85f19a429103e74aee896ca
mngoh <mngoh8@gmail.com>
2025-11-25 15:35:51 -0500

    updates

--- app.py
+++ app.py
@@ -9,15 +9,12 @@ st.set_page_config(
     page_title="Interactive Dashboard", 
     page_icon="🧊",
     layout="wide",
-    initial_sidebar_state="expanded",
-    menu_items={
-        "Get help": "https://example.com/help",
-        "Report a bug": "https://example.com/bug",
-        "About": "This is an interactive dashboard."
-    }
+    initial_sidebar_state="expanded"
 )
 
 # Load Data
+st.write("### To Get Started Upload Your Data Below")         
+
 uploaded_file = st.file_uploader("Upload your CSV file", type=["csv"])
 
 if uploaded_file is not None:
@@ -27,27 +24,26 @@ if uploaded_file is not None:
     
     ### Column 1
     with col1:
-        label, selector, selector2 = st.columns([1,1,1])
-        with label:
-            st.write("### Preview of Data") 
-        with selector:    
-            selected_col = st.selectbox("Select a column to plot", df.columns)
-        with selector2:    
-            count_n = st.selectbox("Count of Categories to Disply", [x for x in range(1,20)])
+        st.write("### Preview of Data")         
         st.dataframe(df.head(10))
 
 
     ### Column 2    
     with col2:
+        selector, selector2 = st.columns([1,1])
+        with selector:    
+            selected_col = st.selectbox("Select a column to plot", df.columns)
+        with selector2:    
+            count_n = st.select_slider("Count of Categories to Display", options=[x for x in range(0,50,5)])
         if selected_col:
-            st.write(f"### Distribution of (Top {count_n} Values): {selected_col}")
-
+            if count_n == 0: 
+                count_n = 1
             # Detect numeric vs categorical
             if pd.api.types.is_numeric_dtype(df[selected_col]):
                 fig, ax = plt.subplots(figsize=(15,6))
-                ax.hist(df[selected_col].dropna().sort_values(ascending=False),
-                        color='skyblue')
-                plt.xticks(rotation=90)
+                histData = df[selected_col].dropna().sort_values(ascending=False)
+                ax.hist(histData, color='skyblue')
+                ax.tick_params(axis='x', labelrotation=30) 
                 ax.set_xlabel(selected_col)
                 ax.set_ylabel("Frequency")
                 ax.set_title(f"Frequency of {selected_col} Feature")
@@ -57,7 +53,7 @@ if uploaded_file is not None:
                 df[selected_col].value_counts(ascending=False)[:count_n].plot(
                     kind="bar", ax=ax, color='skyblue'
                 )
-                plt.xticks(rotation=90)
+                ax.tick_params(axis='x', labelrotation=30) 
                 ax.set_xlabel(selected_col)
                 ax.set_ylabel("Count")
                 ax.set_title(f"Frequency of {selected_col} Feature")
@@ -65,3 +61,14 @@ if uploaded_file is not None:
 
 else:
     st.info("Please upload a CSV file to begin.")
+
+
+
+### Next Section 
+    
+st.write("### Interaction Between Columns")   
+cola, colb = st.columns([1,1])
+with cola:    
+    cola = st.selectbox("Column A", df.columns)
+with colb:    
+    colb = st.selectbox("Column B", df.columns)    
